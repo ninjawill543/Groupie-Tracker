8775d7be669db39d3b3c653d0550e8f8bd5dcbcb
fmael <mael.fath@ynov.com>
2022-12-15 10:00:55 +0100

    server

---
+++ main.go
@@ -0,0 +1,50 @@
+/*
+Lance le serveur
+*/
+
+package main
+
+import (
+	"net/http"
+	"html/template"
+	"fmt"
+)
+
+// Variables envoyées au code html
+type Data struct {
+
+}
+
+func main() {	
+
+	fs := http.FileServer(http.Dir("./static/assets"))
+	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
+	http.HandleFunc("/", Handler)
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+
+	tmpl := template.Must(template.ParseFiles("./static/index.html")) // Affiche la page
+
+	// Affiche dans le terminal l'activité sur le site
+	switch r.Method {
+	case "GET":
+		fmt.Println("GET")
+	case "POST": // Gestion d'erreur
+		if err := r.ParseForm(); err != nil {
+			return
+		}
+	}
+
+
+	// variable := r.Form.Get("input") // Récupère la saisie du joueur
+
+	// Transmet au code html les variables nécessaires
+	data := Data{
+	}
+
+	tmpl.Execute(w, data) // Execute le code html en fonction des changements de variables
+
+}
