0fdf0cadfdd335827d968210961ddc45784bfd60
geoffrey <geoffrey.diederichs@ynov.com>
2022-12-16 10:02:33 +0100

    structures json dans package main

--- api/data.go
+++ api/data.go
@@ -8,49 +8,28 @@ import (
 	"fmt"
 	"net/http"
 	"io/ioutil"
-	"encoding/json"
+	// "encoding/json"
 	"strconv"
 )
 
 // Contient les urls de l'api
 var apiUrls = [4]string{"https://groupietrackers.herokuapp.com/api/artists", "https://groupietrackers.herokuapp.com/api/locations", "https://groupietrackers.herokuapp.com/api/dates", "https://groupietrackers.herokuapp.com/api/relation"}
 
-// Structure pour contenir les données de l'api
-type datasJson struct {
-	artists Artists // Informations générales sur le groupe
-	locations Locations // Lieux des concerts
-	dates Dates // Dates des concerts
-	// relations Relations
-}
-
 // Extrait et retourne les données en bytes de l'url donné
-func ExtractRawData(url string) []byte {
-	resp, err := http.Get(url) // Récupère le contenu de l'url
+func ExtractRawData(urlIndex int) []byte {
+	resp, err := http.Get(apiUrls[urlIndex]) // Récupère le contenu de l'url
 	if err != nil { // En cas d'erreur
-		fmt.Println("Error loading the API : ",url)
+		fmt.Println("Error loading the API : ", apiUrls[urlIndex])
 		panic(err)
 	}
 
 	rawData, err := ioutil.ReadAll(resp.Body) // Récupère les données dans l'html
 	if err != nil { // En cas d'erreur
-		fmt.Println("Error reading the response body : ",url,"\n",resp)
+		fmt.Println("Error reading the response body : ", apiUrls[urlIndex], "\n", resp)
 		panic(err)
 	}
 
 	return rawData // Retourne les données en bytes
-}
-
-// Récupère les données de l'api et les stockent dans une structure datasJson sous forme de json
-func InitializeData() datasJson {
-	var apiData datasJson
-	fmt.Println(apiData.artists)
-	// Récupère les données en tableau de bytes et les formata en Json
-	json.Unmarshal(ExtractRawData(apiUrls[0]), &apiData.artists)
-	json.Unmarshal(ExtractRawData(apiUrls[1]), &apiData.locations)
-	json.Unmarshal(ExtractRawData(apiUrls[2]), &apiData.dates)
-	// json.Unmarshal(ExtractRawData(apiUrls[3]), &apiData.relations)
-
-	return apiData
 }
 
 // Récupère l'id d'un nom de groupe donné
@@ -63,6 +42,7 @@ func GetId(artistsData Artists, name string) int {
 	return -1 // Si aucun groupe avec ce nom n'a été trouvé
 }
 
+/* Inutile ?
 func GetDataById(artistsData Artists, id int) []string {
 	var groupData []string
 	for _, i := range artistsData {
@@ -111,3 +91,4 @@ func GetConcertsById(locationData Locations, dateData Dates, id int) []string {
 
 	return concertsData
 }
+*/
--- main.go
+++ main.go
@@ -6,18 +6,23 @@ package main
 
 import (
 	"net/http"
-	"html/template"
+	// "html/template"
 	"fmt"
 	api "apiFunctions/api"
+	"encoding/json"
 )
-
-// Variables envoyées au code html
-type Data struct {
+ 
+// Structure pour contenir les données de l'api
+type datasJson struct {
+	artists api.Artists // Informations générales sur le groupe
+	locations api.Locations // Lieux des concerts
+	dates api.Dates // Dates des concerts
+	// relations Relations
 }
 
 func main() {
-	data := api.InitializeData()
-	fmt.Println(data)
+	data := InitializeData()
+	fmt.Println(data.artists)
 
 	fs := http.FileServer(http.Dir("./static/assets"))
 	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
@@ -27,7 +32,7 @@ func main() {
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 
-	tmpl := template.Must(template.ParseFiles("./static/index.html")) // Affiche la page
+	// tmpl := template.Must(template.ParseFiles("./static/index.html")) // Affiche la page
 
 	// Affiche dans le terminal l'activité sur le site
 	switch r.Method {
@@ -39,13 +44,26 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-
+	/*
 	// variable := r.Form.Get("input") // Récupère la saisie du joueur
 
 	// Transmet au code html les variables nécessaires
+	
 	data := Data{
 	}
 
 	tmpl.Execute(w, data) // Execute le code html en fonction des changements de variables
+	*/
+}
 
+// Récupère les données de l'api et les stockent dans une structure datasJson sous forme de json
+func InitializeData() datasJson {
+	var apiData api.datasJson
+	// Récupère les données en tableau de bytes et les formata en Json
+	json.Unmarshal(api.ExtractRawData(0), &apiData.artists)
+	json.Unmarshal(api.ExtractRawData(1), &apiData.locations)
+	json.Unmarshal(api.ExtractRawData(2), &apiData.dates)
+	// json.Unmarshal(ExtractRawData(apiUrls[3]), &apiData.relations)
+
+	return apiData
 }
